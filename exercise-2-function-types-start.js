e0ce0aa6242e70cc8a70e8990eeab386b4f1bd6e
Jef <jefersonrevellobiz@gmail.com>
2023-10-02 22:17:27 +0200

    complete exercise

--- exercise-2-function-types-start.js
+++ exercise-2-function-types-start.js
@@ -1,51 +1,32 @@
+"use strict";
 /**
  * START: Follow the instructions below.
  */
-
+Object.defineProperty(exports, "__esModule", { value: true });
 // Add types to this function declaration.
-
-function doubleThePopulation(value: number): number {
-  return value * 2;
+function doubleThePopulation(value) {
+    return value * 2;
 }
-
 // Correct or remove the function calls which cause type errors.
-
 doubleThePopulation(5);
-
 // doubleThePopulation("6.12"); - error
 // correction:
 doubleThePopulation(6.12);
-
-doubleThePopulation(8_526);
-
+doubleThePopulation(8526);
 // doubleThePopulation(false); - error => removed
-
 // Alter this function so the `language2` parameter is optional.
 // Hint: Check `language2` is not `undefined` before passing it to `console.log()`.
-
-function languagesSpoken(
-  country: string,
-  language1: string,
-  language2?: string
-): void {
-  if (typeof language2 === "string") {
-    console.log(`The languages spoken in ${country} are:`);
-
-    console.log(language1);
-    console.log(language2);
-  } else {
-    console.log(`The language spoken in ${country} is:`);
-
-    console.log(language1);
-  }
+function languagesSpoken(country, language1, language2) {
+    if (typeof language2 === "string") {
+        console.log("The languages spoken in ".concat(country, " are:"));
+        console.log(language1);
+        console.log(language2);
+    }
+    else {
+        console.log("The language spoken in ".concat(country, " is:"));
+        console.log(language1);
+    }
 }
-
 languagesSpoken("Colombia", "Spanish", "English");
-
 languagesSpoken("Greece", "Greek");
-
 languagesSpoken("New Zealand", "English", "Māori");
-
-// ----
-
-export {};
